bfa88e88ef1da3b610dcff9ba79da194c50ba6f9
pbwilliams <pbwilliams@uchicago.edu>
2015-02-18 12:56:12 -0600

    Assignment 2
    Two functions: makeCacheMatrix and cacheSolve that together solve the
    inversion of a given matrix, store the solution for later recall, or
    calculate the solution if not cached.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix solves the inversion of the given matrix x and stores the solution
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL 
+        set <- function(y) { 
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve looks up the solution to the inverse matrix and if doesn't exist, solves it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
